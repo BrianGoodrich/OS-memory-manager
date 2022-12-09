77a495d7e508e4b14f2444b493ad57c9d662ae27
brian goodrich <goodrich@hoare7.cs.umsl.edu>
2022-12-09 16:27:34 -0600

    Just realized I hadn't been using git, but at this point I just finished the process termination handling in user and oss. Last running state was that OSS would fork 2 processes and I was just having them continually request stuff and I was granting every request on multiple processes until the program timed out. Was working fine. Current state is that we are now updating frame and page table upon request, handling page faults, placing processes in queue, checking queue if the time is right for the process at the head of the queue, if it is grant that request. Also just granting request if it is already in frame table. Writing to file currently working. Have not tested newly implemented stuff with queue. Commit before testing and implementing FIFO if frame table is full.

---
+++ oss.c
@@ -0,0 +1,476 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+//Shared memory for clock
+#define CLOCK_SHMKEY 511598
+
+#define CLOCK_BUF (sizeof(int) * 2)
+
+//Permissions for msg queue
+#define PERMS 0644
+
+//Struct for the queue so that we can have some data for each item in queue.
+struct blockedProc{
+int process;
+int timeBlocked;
+int request;
+int operation;
+};
+
+//Vars for q
+struct blockedProc procArray[18];
+int front = 0;
+int rear = -1;
+int itemCount = 0;
+
+//Struct for frame table so that we can keep track of if a frame is occupied, dirty bit set. 
+struct frame{
+int occupied;
+int dirty;
+int process;
+int page;
+};
+
+//msgbuf for messages
+struct msgbuf{
+long mtype;
+char mtext[15];
+int request;
+int operation; //1 for read 0 for write.
+int procIndex;
+};
+
+//Queue functions
+static void myhandler(int s);
+bool isEmpty();
+bool isFull();
+int size();
+void enqueue(struct blockedProc);
+struct blockedProc dequeue();
+
+//Main
+int main (int argc, char** argv){
+
+//array of frames that is our frame table.
+struct frame frameTable[256];
+
+//Default occupied to 0 and dirty/process to -1.
+int i;
+for(i = 0; i < 256; i++){
+frameTable[i].occupied = 0;
+frameTable[i].dirty = -1;
+frameTable[i].process = -1;
+frameTable[i].page = -1;
+}
+
+//Array of integers, each index is a process and 1 or 0 means that processes are running or not running.
+int procs[18];
+
+//Default all procs to 0 for not running;
+for(i = 0; i < 18; i++){
+procs[i] = 0;
+}
+
+
+//Array of arrays that symbolizes our page table, we have 18 possible processes, and 32 spaces for each page. The value for each page of a process will be the pages index in the frame table if it is there.
+int pageTable[18][32];
+
+//Default everything in pageTable to -1
+int j;
+for(i = 0; i < 18; i++){
+	for(j = 0; j < 32; j++){
+		pageTable[i][j] = -1;
+	}
+}
+
+int totalProcesses = 0;
+
+int nextSecond = 0;
+
+//Vars for message q
+int msqid;
+int len;
+struct msgbuf buf;
+buf.mtype = 1;
+key_t key;
+system("touch msgq.txt");
+
+//Set up message queue
+if((key = ftok("msgq.txt", 'b')) == -1){
+	perror("ftok");
+	myhandler(1);
+}
+
+if((msqid = msgget(key, PERMS | IPC_CREAT)) == -1){
+	perror("msgget");
+	myhandler(1);
+}
+
+
+//Set 2 second timer
+alarm(2);
+
+//Handle the sigalarm
+signal(SIGALRM, myhandler);
+signal(SIGINT, myhandler);
+
+//Set up file stuff
+int fileLines = 0;
+FILE *filePtr = fopen("output.txt", "w");
+
+if(filePtr == NULL){
+perror("file open error");
+}
+
+
+//Attach to shared memory for clock
+int shmid = shmget(CLOCK_SHMKEY, CLOCK_BUF, 0777 | IPC_CREAT);
+
+if(shmid == -1){
+	perror("error in oss shmget for clock");
+	myhandler(1);
+}
+
+char* paddr = (char*)(shmat(shmid, 0, 0));
+int* clock = (int*)(paddr);
+
+clock[0] = 0;
+clock[1] = 1;
+
+
+
+int x;
+int nextProcessIndex = 0;
+int flag = 1;
+
+
+//Loop looking for memory requests.
+while(1){
+
+	//If we pass 1 billion nanoseconds then increment seconds and roll over nanoseconds.
+	if(clock[1] >= 1000000000){
+		clock[0] += 1;
+		clock[1] = clock[1] - 1000000000;
+	}
+	
+	//If we have reached 100 total processes terminate
+	if(totalProcesses >= 100){
+		myhandler(1);
+		break;
+	}
+
+
+
+	//Generate a new process every second and only generate 1 for now.
+if(clock[0] == nextSecond && totalProcesses < 2){
+		
+		//If there are 18 or more total procs, then we need to check and see if they are all running, if they are we wait, if not we assign that index to the next process we generate.
+		if(totalProcesses >= 18){
+	
+			int x;
+			//Set nextIndex to a large flag.
+			nextProcessIndex = 50;
+			for(x = 0; x < 18; x++){
+				if(procs[x] == 0 ){
+					nextProcessIndex = x;
+					flag = 1;
+					break;
+				}
+			}
+			//If we have gone through the entire loop above and nextProcessIndex was never set then they are all running, so we wait, set flag.
+			if(nextProcessIndex == 50){
+				wait(NULL);
+				flag = 0;
+
+			}
+		}
+//If the flag is 1 that means we have an index available and we will generate a new process.
+	if(flag == 1){
+		if(fileLines < 10000){
+			fprintf(filePtr, "\n\nTotal processes currently = %d launching new process\n\n\n", totalProcesses);
+			fileLines += 6;
+		}
+	
+		char charIndex[1];
+		
+		procs[nextProcessIndex] = 1; //Set this so that now this process is set to running with value of 1.		
+		
+		sprintf(charIndex, "%d", nextProcessIndex);
+
+		pid_t childPid = fork();
+
+		if(childPid == -1){
+        	perror("failed to fork in oss");
+        	myhandler(1);
+		}
+
+		if(childPid == 0){
+        		char* args[] = {"./user", charIndex, 0};
+        		execvp(args[0], args);
+		}
+
+		totalProcesses++;
+		nextProcessIndex++;
+		nextSecond++;
+
+	}	
+}
+
+
+//Now that we have potentially created either our first process or a new process we want to do a msgrcv so that we wait to get a request from one of these processes. Only do this if there is at least 1 process so we dont get stuck here.
+if(totalProcesses > 0){
+
+	//Before we recieve the next message we want to check our queue and see if it is time for the process at the head of the q to recieve its message. 
+	
+	//If the clock has surpassed the time that the process at the head of the queue was placed into the queue + disk read/write time
+	if((procArray[front].timeBlocked + 14000000) <= clock[1]){
+		//We have already placed this requested page in the frame table so just need to send a message to this process, remove it from queue, and continue the loop.
+		struct blockedProc returnedProc = dequeue();		
+		
+		buf.mtype = returnedProc.process;
+		
+		if(msgsnd(msqid, &buf, sizeof(struct msgbuf), 0) == -1)
+                	perror("in oss msgsend");		
+
+	
+		continue;
+	}
+
+
+	if(msgrcv(msqid, &buf, sizeof(struct msgbuf), 20, 0) == -1){
+		perror("msgrcv in oss");
+		myhandler(1);
+	}
+		
+	//Right when we recieve the message we want to check and see if the process terminated
+	if(buf.request == 99){
+		fprintf("Process %d has terminated at time %d sec %d ns\n", buf.procIndex, clock[0], clock[1]);
+	
+		procs[buf.procIndex] = 0;
+		//Loop through the processes page table and free blocks in frame table.
+		int x;
+		for(x = 0; x < 32; x++){
+			//If the page has a frame index reset pageTable index back to defaults and remove it from frame table too.
+			if(pageTable[procIndex][x] != -1){
+				frameTable[pageTable[procIndex][x]].occupied = 0;	
+				frameTable[pageTable[procIndex][x]].dirty = -1;
+				frameTable[pageTable[procIndex][x]].process = -1;
+				frameTable[pageTable[procIndex][x]].page = -1;
+
+				pageTable[procIndex][x] = -1;
+			}				
+
+
+		}
+	
+		//Now that we reset that processes page table and the frames that it had taken up we want to reset the loop so we can recieve a different message.
+		continue;
+	}
+
+	
+
+	if(fileLines < 10000 && buf.procIndex != 0){
+        fprintf(filePtr,"Process %d has requested operation %d to memory location %d\n", buf.procIndex, buf.operation, buf.request);
+        fileLines++;
+        }
+	
+	//Divide request by 1000 to get page number in the page table.
+	int pageNum = (buf.request / 1000);
+	
+	//If the page isnt in the frame table, we have a page fault
+	//Need to place process in queue, then place that page in frame table.
+	if(pageTable[buf.procIndex][pageNum] == -1){
+		if(fileLines < 10000){
+		fprintf(filePtr, "Page fault on process %d page %d", buf.procIndex, pageNum);
+		}
+		
+		//Loop through frame table finding next available spot.
+		int x;
+		for(x = 0; x < 256; x++){
+			
+			//If frame in table isn't occupied then we set it and place the index in the page table also.
+			if(frameTable[x].occupied == 0){
+				frameTable[x].occupied = 1;
+				//Set dirty bit if write
+				if(buf.operation == 0){
+					frameTable[x].dirty = 1;
+			
+				}						
+				frameTable[x].page = pageNum;
+				frameTable[x].process = buf.procIndex;
+				pageTable[buf.procIndex][pageNum] = x;					
+				break;		
+			}	
+		}
+		
+		//Now put the process in the queue and continue the larger while loop.
+		//Build blocked process to be put into the queue
+		struct blockedProc tempProc;
+
+		tempProc.process = buf.procIndex;
+		tempProc.timeBlocked = clock[1];
+		tempProc.request = pageNum;
+		tempProc.operation = buf.operation;
+		
+		//Queue the process that page faulted then continue the main loop starting it over. If we dont continue we will go through the process belowgranting the current process request that is in message buf.
+		enqueue(tempProc);
+		//Add 14ms to the clock
+		
+		if(buf.operation == 0){ //If write operation add more.
+			clock[1] += 20000000;
+		}
+		else
+			clock[1] += 14000000;
+		
+		continue;
+			
+	}
+	else{ //Will occur if the page is already in frame table just grant and add 10ns
+		if(buf.operation == 0){//If write operation add more.
+			clock[1] += 100;
+		}
+		else
+			clock[1] += 10;
+
+		if(fileLines < 10000 && buf.procIndex != 0){
+        	fprintf(filePtr,"Granting process %d operation %d on memory location %d\n", buf.procIndex, buf.operation, buf.request);
+       		fileLines++;
+        	}
+	
+				
+
+		buf.mtype = buf.procIndex + 1;
+		buf.request = 0; //Since requests are from 1-32k we set to 0 when request approved.
+	
+		if(msgsnd(msqid, &buf, sizeof(struct msgbuf), 0) == -1)
+                perror("in oss msgsend");
+
+	}
+}
+
+//Adding overhead for oss process.
+clock[1] += 5000;
+}//End while
+
+shmctl(shmid, IPC_RMID, NULL);
+
+system("rm msgq.txt");
+
+if(msgctl(msqid,IPC_RMID, NULL) == -1){
+		perror("msgctl");
+		myhandler(1);
+}
+
+
+if(fileLines < 10000){
+	fprintf(filePtr,"Program will now terminate\n");
+	fileLines++;
+}
+
+}
+
+static void myhandler(int s){
+
+	int msqid;
+	key_t key;
+
+	if ((key = ftok("msgq.txt", 'b')) == -1){
+                perror("ftok");
+                exit(1);
+        }
+
+	if((msqid = msgget(key, PERMS | IPC_CREAT)) == -1){
+                perror("msgget");
+                exit(1);
+        }
+
+	if(msgctl(msqid, IPC_RMID, NULL) == -1) {
+                perror("msgctl");
+                exit(1);
+        }
+
+	int shmid = shmget(CLOCK_SHMKEY, CLOCK_BUF, 0777);
+	
+	shmctl(shmid, IPC_RMID, NULL);
+	
+	system("rm msgq.txt");
+
+	printf("Program will now terminate from handler.\n");	
+	
+	exit(1);
+}
+
+
+
+//Functions for queue
+
+bool isEmpty(){
+return itemCount == 0;
+}
+
+bool isFull(){
+return itemCount == 18;
+}
+
+int size() {
+return itemCount;
+}
+
+void enqueue(struct blockedProc proc){
+	
+	if(!isFull()){
+		if(rear == 17){
+			rear = -1;
+		}
+	
+		
+		procArray[++rear] = proc;
+		itemCount++;
+	}
+
+}
+
+struct blockedProc dequeue(){
+
+struct blockedProc proc = procArray[front++];
+
+	if(front == 18){
+		front = 0;
+	}
+	
+	itemCount--;
+	return proc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ user.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/msg.h>
+#include <string.h>
+
+//Shared memory for clock
+#define CLOCK_SHMKEY 511598
+
+#define PERMS 0644
+
+//Buffer for clock
+#define CLOCK_BUF (sizeof(int) * 2)
+
+struct msgbuf{
+long mtype;
+char mtext[15];
+int request;
+int operation; //1 for read 0 for write
+int procIndex;
+};
+
+static void myhandler(int s);
+
+int main (int argc, char** argv){
+
+//Vars for message q
+int msqid;
+int len;
+struct msgbuf buf;
+buf.mtype = 1;
+key_t key;
+int index = atoi(argv[1]);
+
+//Set up message q
+if((key = ftok("msgq.txt", 'b')) == -1){
+		perror("ftok");
+		exit(1);
+}
+
+if((msqid = msgget(key, PERMS)) == -1){
+		perror("msgget");
+		exit(1);
+}
+
+
+//Set up timer just in case
+alarm(1);
+signal(SIGALRM, myhandler);
+signal(SIGINT, myhandler);
+
+
+//Attach to shared mem for clock
+int shmid = shmget(CLOCK_SHMKEY, CLOCK_BUF, 0777);
+
+if(shmid == -1){
+	perror("in user shmget for clock");
+	exit(1);
+}
+
+char* paddr = (char*)(shmat(shmid, 0, 0));
+int* clock = (int*)(paddr);
+
+int x = 0;
+int oneK = 1000;
+int termFlag = 0;
+
+printf("I am process: %d\n", index);
+
+while(1){
+
+buf.request = (rand() % 32000 + 1);
+
+int operation = (rand() % 8 + 1);
+
+//If operation is 1-6 treat it as a read, biasing it towards reads 75%. 
+if(operation == 7 || operation == 8){
+	buf.operation = 0; //Write
+}
+else{
+	buf.operation = 1; //Read
+}
+
+buf.mtype = 20;
+
+buf.procIndex = index;
+
+//Roll to see if we should terminate after every 1k memory references. Giving this a 40% chance.
+if(x >= oneK){
+
+	int terminate = (rand() % 10 + 1);
+
+	if(terminate == 1 || terminate == 2 || terminate == 3 || terminate == 4){
+		//If we are terminating send msg to OSS and call handler.
+		buf.request = 99;
+		
+		if(msgsnd(msqid, &buf, sizeof(struct msgbuf), 0) == -1){
+ 		       perror("msgsend");
+		}
+		
+		myhandler(1);
+		
+	}
+	else{
+		termFlag = 1; //Means we reached 1k mem references but didnt terminate so set flag so that we can increment 1k to 2k now.
+	}
+	
+}
+
+if(termFlag == 1){
+oneK += 1000;
+termFlag = 0;
+}
+
+if(msgsnd(msqid, &buf, sizeof(struct msgbuf), 0) == -1){
+	perror("msgsend");
+}
+/*
+if(index > 0){
+printf("Message sent by process %d request is %d\n", index, buf.request);
+}
+*/
+if(msgrcv(msqid, &buf, sizeof(struct msgbuf), (index + 1), 0) == -1){
+        perror("msgrcv");
+        exit(1);
+}
+
+
+/*
+if(index > 0){
+printf("Message received by process %d request is %d\n", index, buf.request);
+}
+*/
+x++;
+}//End while
+
+
+
+
+shmctl(shmid, IPC_RMID, NULL);
+
+}
+
+
+static void myhandler(int s){
+
+int shmid = shmget(CLOCK_SHMKEY, CLOCK_BUF, 0777);
+
+shmctl(shmid, IPC_RMID, NULL);
+
+exit(1);
+}
